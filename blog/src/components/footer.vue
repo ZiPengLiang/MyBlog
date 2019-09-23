0e860ebe38311222a1ed0fcbfe16a4d6d1fb0b63
liangzipeng <liangzipeng@kaytion.com>
2019-09-23 10:06:58 +0800

    备案号

--- blog/src/components/footer.vue
+++ blog/src/components/footer.vue
@@ -1,6 +1,18 @@
 <template >
   <div class="footer">
-    <a href>粤ICP备19112336号</a>
+    <!-- <a href='http://www.beian.gov.cn/portal/registerSystemInfo'>粤ICP备19112336号</a> -->
+    <div style="width:300px;margin:0 auto; padding:20px 0;">
+      <a
+        target="_blank"
+        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44060602001398"
+        style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"
+      >
+        <img src="../assets/recordIcon.png" style="float:left;" />
+        <p
+          style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;"
+        >粤公网安备 44060602001398号</p>
+      </a>
+    </div>
   </div>
 </template>
 <script>
